8540c2e2891ef947b3685b05b15fcbea23aa9515
jumbo12367 <jumbonahotdog098@gmail.com>
2025-04-02 23:06:56 +0800

    Add files via upload

---
+++ about/about.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const joinSection = document.getElementById('join');
+    joinSection.addEventListener('click', function() {
+        alert("You're ready to dive into the games! Enjoy!");
+    });
+});
