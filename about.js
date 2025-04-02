e7f7d865d35c91ae8dd3136bbf704f7f9405ec4c
jumbo12367 <jumbonahotdog098@gmail.com>
2025-04-02 23:05:27 +0800

    Delete about.js

--- about.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const joinSection = document.getElementById('join');
-    joinSection.addEventListener('click', function() {
-        alert("You're ready to dive into the games! Enjoy!");
-    });
-});
